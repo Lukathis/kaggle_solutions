83ac6bca0adc82f96ebfe8532602aa4cad836fd7
Lukathis <xuchi16@126.com>
2017-06-22 23:05:08 +0800

    6-22

--- iris/visualization.py
+++ iris/visualization.py
@@ -9,14 +9,36 @@ import seaborn as sns
 #
 # read in the iris dataset
 iris = pd.read_csv("/Users/Chi/PycharmProjects/kaggle_solutions/iris/data/iris.csv")
-iris.head()
+print(iris.head())
 
-print(iris["Species"].value_counts())
-iris.plot(kind="scatter", x="SepalLengthCm", y="SepalWidthCm")
-sns.jointplot(x="SepalLengthCm", y="SepalWidthCm", data=iris, size=5)
-sns.FacetGrid(iris, hue="Species", size=5).map(plt.scatter, "SepalLengthCm", "SepalWidthCm").add_legend()
-sns.boxplot(x="Species", y="PetalLengthCm", data=iris)
-ax = sns.boxplot(x="Species", y="PetalLengthCm", data=iris)
-ax = sns.stripplot(x="Species", y="PetalLengthCm", data=iris, jitter=True, edgecolor="gray")
+''' 查看iris以及子集 '''
+# print(iris)
+# print(iris['SepalLengthCm'])
+# print(iris[['SepalLengthCm', 'SepalWidthCm']])
 
+print(iris.describe())
+
+''' 绘制除了ID列之外其他列的范围分布boxplot '''
+# iris_features = iris[['SepalLengthCm', 'SepalWidthCm', 'PetalLengthCm', 'PetalWidthCm']];
+# iris_features.boxplot()
+
+''' 绘制散点图 '''
+plt.scatter(iris['PetalWidthCm'], iris['PetalLengthCm'], alpha=1.0, color='k')
+plt.xlabel('Petal Width')
+plt.ylabel('Petal Length')
+plt.show()
+
+''' 绘制直方图 '''
+plt.hist(iris['PetalWidthCm'], bins=20)
+plt.xlabel('petal width distribution')
+
+
+# print(iris["Species"].value_counts())
+# iris.plot(kind="scatter", x="SepalLengthCm", y="SepalWidthCm")
+# sns.jointplot(x="SepalLengthCm", y="SepalWidthCm", data=iris, size=5)
+# sns.FacetGrid(iris, hue="Species", size=5).map(plt.scatter, "SepalLengthCm", "SepalWidthCm").add_legend()
+# sns.boxplot(x="Species", y="PetalLengthCm", data=iris)
+# ax = sns.boxplot(x="Species", y="PetalLengthCm", data=iris)
+# ax = sns.stripplot(x="Species", y="PetalLengthCm", data=iris, jitter=True, edgecolor="gray")
+#
 plt.show()
